723ed362d4b773ff793aa5a24d195d55302aad4b
ratwoods <56761295+ratwoods@users.noreply.github.com>
2021-01-02 15:49:10 +0100

    add console click

--- src/App.js
+++ src/App.js
@@ -32,6 +32,11 @@ class App extends Component {
   onInputChange = (event) => {
     console.log(event.target.value);
   }
+
+  onButtonSubmit = () =>{
+    console.log('click')
+  }
+
   render(){ 
     return (
       <div className="App">
@@ -42,7 +47,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange}/>
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
                {/* <FaceRecognition />*/}         
       </div>
     );
