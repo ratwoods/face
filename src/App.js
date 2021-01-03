bb49916ecbb40ac2571d442d768a1e2084ae3ad1
ratwoods <56761295+ratwoods@users.noreply.github.com>
2021-01-03 18:31:21 +0100

    register

--- src/App.js
+++ src/App.js
@@ -36,7 +36,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -72,18 +73,24 @@ class App extends Component {
   }
 
   onRouteChange =(route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
     this.setState({route: route});
   }
 
   render(){ 
+    const {isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <Particles 
           className='particles'
           params={particlesOptions}
         />
-        <Navigation onRouteChange={this.onRouteChange} />
-        { this.state.route === 'home' 
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        {route === 'home' 
           ?  <div>
               <Logo />
               <Rank />
@@ -91,10 +98,10 @@ class App extends Component {
                 onInputChange={this.onInputChange} 
                 onButtonSubmit={this.onButtonSubmit}
               />
-              <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/> 
+              <FaceRecognition box={box} imageUrl={imageUrl}/> 
             </div>
           :(
-            this.state.route === 'signin' 
+            route === 'signin' 
             ? <Signin onRouteChange={this.onRouteChange} />
             : <Register onRouteChange={this.onRouteChange} />
             )
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const Navigation = ({onRouteChange }) => {
-	return (
-		<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-			<p onClick={() => onRouteChange('signin')} className='rat f3 link dim black underline pa3 mointer'>Sign out</p>
-		</nav>
-	);
+const Navigation = ({onRouteChange, isSignedIn }) => {
+		if (isSignedIn) {
+			return(
+			  <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+					<p onClick={() => onRouteChange('signout')} className='rat f3 link dim black underline pa3 mointer'>Sign out</p>
+				</nav>
+			);
+		} else {
+			return(
+				<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+					<p onClick={() => onRouteChange('signin')} className='rat f3 link dim black underline pa3 mointer'>Sign in</p>
+					<p onClick={() => onRouteChange('Register')} className='rat f3 link dim black underline pa3 mointer'>Register</p>
+				</nav>
+			);
+		}
 }
 
 export default Navigation;
