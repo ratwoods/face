8aa1abcb217c1684396c8db362f0600d3080f7ff
ratwoods <56761295+ratwoods@users.noreply.github.com>
2021-01-03 14:26:23 +0100

    back to normal

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -3,12 +3,12 @@ import './FaceRecognition.css';
 
 const FaceRecognition = ({ imageUrl, box }) => {
 	return (
-		<dev className='center ma' >
+		<div className='center ma' >
 			<div className='absolute mt2'>
 			  <img id='inputimage' alt= ''  src={imageUrl} width='500px' heigh='auto' />
 			  <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }} ></div>
 			</div>
-		</dev>
+		</div>
 	);
 }
 
